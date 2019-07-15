955507063f7ac6fc7994afdc0b94b698db4b9dda
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2019-07-15 14:50:01 -0300

    slider fixes

--- src/app/components/sliders/sliders.component.ts
+++ src/app/components/sliders/sliders.component.ts
@@ -14,7 +14,7 @@ export class SlidersComponent implements OnInit, AfterViewInit {
     end: number;
     range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
     checkbox = false;
-    ballon = true;
+    ballon = false;
     checkboxDark = true;
     ballonDark = true;
     value: any;
