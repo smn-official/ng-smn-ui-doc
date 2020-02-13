404c16dde1cc55762b034f96dc428e5e7fb92c19
Claudanilo Xavier <claudanilo@smn.com.br>
2020-02-13 11:20:45 -0300

    Doc avatar finalizada

--- src/app/main/components/avatar/avatar.component.ts
+++ src/app/main/components/avatar/avatar.component.ts
@@ -1,23 +1,66 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {UiToolbarService} from "ng-smn-ui";
+import {Title} from "@angular/platform-browser";
+import {TranslateService} from "@shared/translate/translate.service";
 
 @Component({
     selector: 'app-avatar',
     templateUrl: './avatar.component.html',
     styleUrls: ['./avatar.component.scss']
 })
-export class AvatarComponent implements OnInit {
-
+export class AvatarComponent implements OnInit, AfterViewInit {
     info: any;
+    translate: any;
 
-    constructor() {
+    constructor(private toolbarService: UiToolbarService,
+                private titleService: Title,
+                private translateService: TranslateService ) {
+        this.translate = {
+            labels: {}
+        };
         this.info = {
-            name: 'Jhon Doe',
-            color: '#69F0AE',
-            image: 'http://gameranx.com/wp-content/uploads/2016/02/No-Mans-Sky-4-K-Wallpaper.jpg'
+            name: 'Barry Allen',
+            color: '#F44336',
+            image: 'https://imagensemoldes.com.br/wp-content/uploads/2018/03/The-Flash-Logo-Vetor-e-PNG.png'
         };
     }
 
-    ngOnInit() {
+    async ngOnInit() {
+        this.translate = await this.translateService.getLanguageData('avatar');
     }
 
+    ngAfterViewInit(): void {
+        this.titleService.setTitle('Avatar');
+        this.toolbarService.set('Avatar');
+    }
+
+    getHtml() {
+        return `<!-- With picture -->
+<ui-avatar [image]="info.image"
+                   [name]="info.name"
+                   [color]="info.color"
+                   [size]="80"></ui-avatar>
+
+<!-- Without picture -->
+<ui-avatar [name]="info.name"
+                   [color]="info.color"
+                   [size]="50"></ui-avatar>
+`;
+    }
+
+    getTypescript() {
+        return `import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-avatar',
+  templateUrl: 'demo-avatar.component.html',
+  styleUrls: ['demo-avatar.component.scss'],
+})
+export class DemoAvatarComponent {
+  image: string;
+  name: string;
+  color: string;
+  size: number;
+}`;
+    }
 }
