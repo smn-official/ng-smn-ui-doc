75409ed3511029a746069dd6641495adb738a2fe
Leticia Faleiros <leticiafaleiros9@gmail.com>
2020-02-27 10:25:33 -0300

    Dividers doc

--- src/app/main/components/dividers/dividers.component.ts
+++ src/app/main/components/dividers/dividers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import { Title } from '@angular/platform-browser';
 import { UiToolbarService } from 'ng-smn-ui';
+import { TranslateService } from '../../../shared/translate/translate.service';
 
 @Component({
     selector: 'app-dividers',
@@ -9,18 +10,38 @@ import { UiToolbarService } from 'ng-smn-ui';
     styleUrls: ['./dividers.component.scss']
 })
 export class DividersComponent implements OnInit, AfterViewInit {
-
+    translate: any;
     constructor(
         private titleService: Title,
-        private toolbarService: UiToolbarService
-    ) { }
+        private toolbarService: UiToolbarService,
+        private translateService: TranslateService) {
+        this.translate = {
+            labels: {}
+        };
+        this.translateService.change.subscribe(async () => {
+            this.translate = await this.translateService.getLanguageData('dividers');
+        });
+    }
 
-    ngOnInit() {
+    async ngOnInit() {
+        this.translate = await this.translateService.getLanguageData('dividers');
     }
 
     ngAfterViewInit() {
         this.titleService.setTitle('Dividers');
         this.toolbarService.set('Dividers');
     }
-
+    getHtml() {
+        return `
+    <div class="light">
+            <ui-card-content>
+                <div class="ui-list">
+                    <a class="ui-list-item" uiRipple>Item 1</a>
+                    <div class="ui-divider"></div>
+                    <a class="ui-list-item" uiRipple>Item 2</a>
+                </div>
+            </ui-card-content>
+    </div>
+    `;
+}
 }
